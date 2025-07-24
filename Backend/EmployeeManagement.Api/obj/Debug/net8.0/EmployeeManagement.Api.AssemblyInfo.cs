6eedf5ec8e03d332e12675489de1315aabb835fc
Henry Chimborazo <henrychimborazo331@gmail.com>
2025-07-24 17:54:34 -0500

    Agregar funcionalidad test unitario

--- Backend/EmployeeManagement.Api/obj/Debug/net8.0/EmployeeManagement.Api.AssemblyInfo.cs
+++ Backend/EmployeeManagement.Api/obj/Debug/net8.0/EmployeeManagement.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2777536995f2fadd9131e78e167f3dd5de33738c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd05c20629d77065a7241f1d9aa877bb69e6a2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Backend/EmployeeManagement.Tests/TestEmployee.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using AutoMapper;
+using EmployeeManagement.Api.Controllers;
+using EmployeeManagement.Domain;
+using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeManagement.Tests
+{
+    public class EmployeesControllerTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnsOkResult()
+        {
+            var repoMock = new Mock<IEmployeeRepository>();
+            repoMock.Setup(r => r.GetAllEmployeeAsync())
+                    .ReturnsAsync(new List<Employee>());
+
+            var storeRepoMock = new Mock<IStoreRepository>();
+
+            var mapperMock = new Mock<IMapper>();
+
+            var controller = new EmployeesController(repoMock.Object, storeRepoMock.Object, mapperMock.Object);
+
+            var result = await controller.GetAll();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
