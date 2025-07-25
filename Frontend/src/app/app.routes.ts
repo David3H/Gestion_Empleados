76ab0902ab4aa0d47fd650fbe806ec9f214aa267
Henry Chimborazo <henrychimborazo331@gmail.com>
2025-07-25 10:28:32 -0500

    Configuración rutas públicas y privadas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './employees/list/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './employees/form/employee-form/employee-form.component';
-import { authGuard as EmployeeAuthGuard } from './employees/guards/auth.guard';
+import { LoginComponent } from './login/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-   { path: '', component: EmployeeListComponent },
-  { path: 'create', component: EmployeeFormComponent, canActivate: [EmployeeAuthGuard] },
-  { path: 'edit/:id', component: EmployeeFormComponent, canActivate: [EmployeeAuthGuard] },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: '', canActivate: [authGuard], children: [
+      { path: '', component: EmployeeListComponent,pathMatch: 'full' },
+      { path: 'create', component: EmployeeFormComponent },
+      { path: 'edit/:id', component: EmployeeFormComponent }
+    ]
+  },
+  { path: '**', redirectTo: ''}
 ];
