4cf9ad5ce20b91d75bfbeb5cc63a6bb84b51599d
Henry Chimborazo <henrychimborazo331@gmail.com>
2025-07-25 10:09:06 -0500

    Implementación guard para manejo de rutas protegidas

--- Frontend/src/app/core/guards/auth.guard.ts
+++ Frontend/src/app/core/guards/auth.guard.ts
@@ -1,5 +1,10 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../../employees/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  return auth.isLoggedIn() || router.parseUrl('/login');
 };
